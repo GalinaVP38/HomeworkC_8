6d47ed1f565c43c7897007063c3c126af206c789
Galina <pgv21@yandex.ru>
2022-09-18 13:55:41 +0300

    save1

--- Seminar8_Task54/Program.cs
+++ Seminar8_Task54/Program.cs
@@ -9,22 +9,32 @@ for (int i=0; i<matrix.GetLength(0); i++)
 {
     for (int j=0; j<matrix.GetLength(1); j++)
     {
-        matrix [i,j] = new Random().Next(0,21);
+        matrix [i,j] = new Random().Next(0,11);
         Console.Write(matrix[i,j]+"\t");
     }
     Console.WriteLine();
 }
 Console.WriteLine("Массив с упорядоченными строками: ");
 
-int [,] tempMatrix = new int [4,4];
+int [,] twoArray = new int [4,4];
 
-for (int i=0; i<matrix.GetLength(0); i++)
+for (int i=0; i < matrix.GetLength(0); i++)
 {
     for (int j=0; j<matrix.GetLength(1); j++)
     {
-        int maxNum=matrix[0,j];
-    
-
+         for (int k = 0; k < matrix.GetLength(1)-1; k++)
+         {
+            for (int l=0; l < matrix.GetLength(1)-1; l++)
+            {
+                if (matrix[i, l] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, l];
+                    matrix[i, l] = temp;
+                }
+            }
+         }
+         Console.Write(matrix[i,j]+"\t");           
     }
-    
+   Console.WriteLine(); 
 }
